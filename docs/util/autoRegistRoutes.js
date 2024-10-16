5ce24b34b37ded5572f2a5f5c1647fb7979c30cc
yang.jinpeng <204138282@qq.com>
2024-10-16 15:01:45 +0800

    init

---
+++ docs/util/autoRegistRoutes.js
@@ -0,0 +1,13 @@
+export const autoRegistRoutes = () => {
+  const modules = [];
+  const files = require.context('../router/module', false, /(config).json$/);
+  files.keys().forEach(filePath => {
+    let context = files(filePath);
+    let objItem = {};
+    objItem.path = context.routeInfo.path;
+    objItem.name = context.routeInfo.name;
+    objItem.routes = context.context;
+    modules.push(objItem);
+  });
+  return modules;
+};
